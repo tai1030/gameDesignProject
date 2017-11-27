f7c6f76465a3103a8c882b3034c290b016a414ad
Brian <s576kks@gmail.com>
2017-11-27 18:51:46 +0800

    Map resize to 15*15 game scene with obstacle

--- Assets/Scripts/map.cs
+++ Assets/Scripts/map.cs
@@ -4,26 +4,43 @@ using UnityEngine;
 
 public class map : MonoBehaviour {
 
+	public GameObject brick;
     public GameObject box;
 
-    private List<GameObject> boxes;
+	private List<GameObject> bricks;
+	private List<GameObject> boxes;
 
 	// Use this for initialization
 	void Start () {
-        boxes = new List<GameObject>();
-		float bx = 0.5f;
-		float bz = 0.5f;
-		for (float i = 0; i <= 25; i++)
+		bricks = new List<GameObject>();
+		for (int i = 0; i <= 14; i++)
         {
-			GameObject box1 = Instantiate(box, new Vector3(1+bx, 0, -i+bz), Quaternion.identity);
-			GameObject box2 = Instantiate(box, new Vector3(-25+bx, 0, -i+bz), Quaternion.identity);
-			GameObject box3 = Instantiate(box, new Vector3(-i+bx, 0, bz), Quaternion.identity);
-			GameObject box4 = Instantiate(box, new Vector3(-i+bx, 0, -25+bz), Quaternion.identity);
-            boxes.Add(box1);
-            boxes.Add(box2);
-            boxes.Add(box3);
-            boxes.Add(box4);
+			GameObject brick1 = Instantiate(brick, new Vector3(1, 0.5f, -i), Quaternion.identity);
+			GameObject brick2 = Instantiate(brick, new Vector3(-14, 0.5f, -i), Quaternion.identity);
+			GameObject brick3 = Instantiate(brick, new Vector3(-i, 0.5f, 0), Quaternion.identity);
+			GameObject brick4 = Instantiate(brick, new Vector3(-i, 0.5f, -15), Quaternion.identity);
+			bricks.Add(brick1);
+			bricks.Add(brick2);
+			bricks.Add(brick3);
+			bricks.Add(brick4);
         }
+
+		GameObject brick5 = Instantiate(brick, new Vector3(1, 0.5f, -15), Quaternion.identity);
+		GameObject brick6 = Instantiate(brick, new Vector3(-14, 0.5f, -15), Quaternion.identity);
+		bricks.Add(brick5);
+		bricks.Add(brick6);
+
+		for (int i = 1; i <= 4; i++) {
+			GameObject brick7 = Instantiate(brick, new Vector3(-2, 0.5f, -i * 3), Quaternion.identity);
+			GameObject brick8 = Instantiate(brick, new Vector3(-5, 0.5f, -i * 3), Quaternion.identity);
+			GameObject brick9 = Instantiate(brick, new Vector3(-8, 0.5f, -i * 3), Quaternion.identity);
+			GameObject brick10 = Instantiate(brick, new Vector3(-11, 0.5f, -i * 3), Quaternion.identity);
+
+			bricks.Add(brick7);
+			bricks.Add(brick8);
+			bricks.Add(brick9);
+			bricks.Add(brick10);
+		}
 	}
 	
 	// Update is called once per frame
