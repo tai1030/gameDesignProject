49cd9ff1963acb98589d78da8e0a5885dbdacce0
Alan Yiu <w40388016@gmail.com>
2017-11-30 21:28:37 +0800

    item box detection

--- Assets/Scripts/BrickController.cs
+++ Assets/Scripts/BrickController.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BrickController : MonoBehaviour {
-    public enum ITEM { KEY, SPEED, FIRE, HP };
-    public ITEM item;
+    public enum ITEM { KEY, SPEED, FIRE, HP, NULL };
+    public ITEM item = ITEM.NULL;
+    private bool isItemActive = false;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +14,69 @@ public class BrickController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Boy")
+        {
+            if(isItemActive){
+                switch (item)
+                {
+                    case ITEM.KEY:
+                        //do Something
+                        break;
+                    case ITEM.SPEED:
+                        //do Something
+                        break;
+                    case ITEM.FIRE:
+                        //do Something
+                        break;
+                    case ITEM.HP:
+                        //do Something
+                        break;
+                }
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(item);
+        if(item != ITEM.NULL){
+            if (other.gameObject.name == "Explosion(Clone)")
+            {
+                if(isItemActive){
+                    Destroy(this.gameObject);
+                }else{
+                    StartCoroutine(setItemActive());
+                }
+            }
+        }
+    }
+
+    IEnumerator setItemActive()
+    {
+        switch (item)
+        {
+            case ITEM.KEY:
+                GetComponent<Renderer>().material.color = new Color(0, 128, 0);//green
+                break;
+            case ITEM.SPEED:
+                GetComponent<Renderer>().material.color = new Color(0, 0, 255);//blue
+                break;
+            case ITEM.FIRE:
+                GetComponent<Renderer>().material.color = new Color(255, 165, 0);//orange
+                break;
+            case ITEM.HP:
+                GetComponent<Renderer>().material.color = new Color(255, 0, 0);//red
+                break;
+            case ITEM.NULL:
+                break;
+        }
+        yield return new WaitForSeconds(1);
+        isItemActive = true;
+    }
 }
